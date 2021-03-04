803b7ead6cf953f26ae569cb1746cf20a420148f
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-03-04 18:25:55 -0500

    setting headers/html

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const http = require('http') // http a core model that launchs a server , sends 
 
 // }
 
-const server = http.createServer((req, res) => {
-    console.log(req)
-}) // creates a server 
+const server = http.createServer((req, res) => {    // creates a server
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body><h1>Hello</h1></body>')
+    res.write('</html>')
+    res.end()
+    // process.exit()  
+})  
 
 server.listen(3000)
+// 
+// node app.js to spin a server 
+// Event Loop

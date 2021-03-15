7ef4fcc8032daae2a4b998dc1912fb141b388fbb
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-03-15 16:28:50 -0400

    using ejs templete engine with 404 page

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ app.use(adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', {pageTitle: 'Page Not Found'})
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
     // res(app.use(routes)).get('/').end() 
 })
 
-app.set('view engine' , 'pug')
+
+app.set('view engine' , 'ejs') // adding ejs 
 app.set('views' , 'views')
 
 
